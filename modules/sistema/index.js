8654b0073f4f35379f3dfecf083743f939a21dc2
Gabriel Portugal <gabrielguadelupe@hotmail.com>
2020-10-11 14:45:16 -0300

    pegar peso via post e salvar no firebase

--- modules/sistema/index.js
+++ modules/sistema/index.js
@@ -27,34 +27,49 @@ module.exports = class module {
     let body = req.body
     console.log('nodejs body', body)
 
-    let ref = `/teste/botija`
+    let ref = `/teste/botija/-MGjXT_YtK0i6AG9BLjh`
     Firebase.database()
       .ref(ref)
-      .on('value', (snap) => {
+      .once('value', (snap) => {
         snap = snap.val();
         console.log(snap);
-      })
+        // verificar variação do peso
 
-    // Firebase.database().ref(`estacao/${body.estacao}/cmd`)
-    // .update(json,(err)=>{
-    //   console.log('subiu para fire', json)
-    //   if(err) {
-    //     console.log('***************');
-    //     console.log('***************');
-    //     console.log('ERROR');
-    //     console.log(err);
-    //     console.log('***************');
-    //     console.log('***************');
-    //   }
-    //   let date = new Date();
-    //   let time = date.getTime();
-    //   res.status(200).json({
-    //     status:200,
-    //     time: time,
-    //     msg: "valor do gás anotado",
-    //     json: json
-    //   });
-    // });
+        let dt = new Date();
+        let created_at = `${dt.getDate()}/${dt.getMonth() + 1}/${dt.getFullYear()} - ${dt.getHours()}:${dt.getMinutes()}:${dt.getSeconds()}`;
+        let created_at_time = dt.getTime();
+
+        let consumo = snap.pesoAtual - body.peso
+
+        let novoPeso = parseInt(body.peso).toFixed()
+        let novaPorcentagem = ((novoPeso * 100) / snap.botija).toFixed()
+        let consumoPorcentagem = (snap.porcentagemAtual - novaPorcentagem).toFixed()
+
+        Firebase.database().ref(ref)
+          .update({
+            pesoAtual: novoPeso,
+            porcentagemAtual: novaPorcentagem
+          }, (err) => {
+          });
+
+        Firebase.database().ref(ref + '/consumo')
+          .push({
+            created_at: created_at,
+            created_at_time: created_at_time,
+            consumo: consumo,
+            porcentagemConsumida: consumoPorcentagem,
+            pesoAtual: novoPeso,
+            porcentagemAtual: novaPorcentagem
+          }, (err) => {
+          });
+
+        res.status(200).json({
+          status: 200,
+          time: created_at,
+          msg: "valor do gás anotado"
+        });
+        return;
+      })
   }
 
 }
